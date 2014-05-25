8a3cd4281bc5cc9e83628716d8ffb5339dfa811b
Vladislav Galyanin <v.galyanin@gmail.com>
2014-05-25 15:47:51 +0400

    Initial commit

---
+++ InlineEdit.php
@@ -0,0 +1,73 @@
+<?php
+
+class InlineEdit {
+	/**
+	 * Echoes or returns inline editable textArea field.
+     * Can be disabled by setting $enable to false.
+     * After user edit, textarea on lost focus (blur) sends post ajax request on
+     * url: /<model class name>/update/<model primary key value> (can be 
+     * overwritten by setting $updateUrl). Adjust view of textarea with
+     * .inline-edit-field, .inline-edit-field.inactive, 
+     * .inline-edit-field.active classes.
+	 * @param CActiveRecord $model model which fields need to be inline editable
+	 * @param string $field name of field which need to be saved after edit
+	 * @param strign $value current value of the field (init value for textarea)
+     * @param bool $enable
+     * @param bool $return whether the rendering result should be returned
+     * instead of being displayed to end users
+	 * @param string $updateUrl ajax POST requests will be sended to this url
+     * @return string the rendering result. 
+     * Null if the rendering result is not required.
+	 */  
+	public static function textField($model, $field, $value, $enable = true, $return = false, $updateUrl = '/{className}/update/{primaryKey}') {
+		if (!$enable)
+            $fieldHtml = $value;
+        else {
+            static $registered;
+            if (!isset($registered[get_class($model)][$field]))
+                $registered[get_class($model)][$field] = self::registerJS(get_class($model), $field, $updateUrl);
+            $fieldHtml = 
+                '<textarea id="inline-edit-'.$model->primaryKey.'" class="inline-edit-field inactive '.get_class($model).'-'.$field.'">'
+                    .$value.
+                '</textarea>';
+        }
+        if($return)
+			return $fieldHtml;
+        else
+			echo $fieldHtml;
+	}
+    
+    /**
+     * Binds .inactive and .active selectors with appropriate functions:
+     * class changing on focus and making ajax request on blur. Must be called 
+     * once for each unique pair $modelName and $fieldName.
+     * @param string $modelName
+     * @param string $fieldName
+     * @param string $updateUrl
+     * @return CClientScript for chaining support
+     */
+    protected static function registerJS($modelName, $fieldName, $updateUrl) {
+        return Yii::app()->clientScript->registerScript(
+            'inlineEdit', 
+            '$(".'.$modelName.'-'.$fieldName.'.inactive").live("focus", function() {
+                $(this).removeClass("inactive");
+                $(this).addClass("active");
+            });
+            $(".'.$modelName.'-'.$fieldName.'.active").live("blur", function() {
+                var updateUrl = "'.$updateUrl.'";
+                $(this).removeClass("active");
+                $(this).addClass("inactive");
+                var primaryKey = $(this).attr("id").replace("inline-edit-","");
+                updateUrl = updateUrl.replace("{className}", "'.$modelName.'").replace("{primaryKey}", primaryKey);
+                var values = {};
+                values["'.$modelName.'['.$fieldName.']"] = $(this).val();
+                $.ajax({
+                    type: "POST",
+                    url: updateUrl,
+                    data: values
+                });
+            });',
+            CClientScript::POS_READY
+        );
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,43 @@ yii-inlineedit
 ==============
 
 Provides simple opportunity for replacing model field values with editable textareas. New values sends via AJAX. Several customization available.
+
+Requirements 
+=========================
+
+Yii 1.1 or above and jQuery.
+
+Installation
+========================= 
+
+To use this extension:
+
+1) copy InlineEdit.php to your components directory
+
+2) check that your config have autoloaded
+
+	'import'=>array(
+		...
+		'application.components.*',
+		...
+		
+Usage
+=========================
+
+Assuming that you have $model - Article activerecord class object with field title and primaryKey = 1 and you need it to be inline editable in your view:
+
+	echo InlineEdit::textField($model, 'title', $model->title);
+
+You can disable such behavior, just echoing $value (for example if you need to show textares only for admins)
+
+	echo InlineEdit::textField($model, 'title', $model->title, $enable);
+
+in such case inline editable textareas will be shown only if $enable == true.
+
+If you need just string value, without sending it to output set $return to true:
+
+	$output = InlineEdit::textField($model, 'title', $model->title, true, true);
+
+Default ajax url setted to Article/update/1 but you always can change it:
+
+	echo InlineEdit::textField($model, 'title', $model->title, true, false, '/<your new url for ajax save call>/');
