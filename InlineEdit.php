396abb451bd146ec72ab3743d523a7274b1ee640
Vladislav Galyanin <v.galyanin@gmail.com>
2014-05-25 15:52:37 +0400

    Indents changed for tab = 4 spaces to tab = 8 spaces

--- InlineEdit.php
+++ InlineEdit.php
@@ -1,26 +1,26 @@
 <?php
 
 class InlineEdit {
-	/**
-	 * Echoes or returns inline editable textArea field.
+    /**
+     * Echoes or returns inline editable textArea field.
      * Can be disabled by setting $enable to false.
      * After user edit, textarea on lost focus (blur) sends post ajax request on
      * url: /<model class name>/update/<model primary key value> (can be 
      * overwritten by setting $updateUrl). Adjust view of textarea with
      * .inline-edit-field, .inline-edit-field.inactive, 
      * .inline-edit-field.active classes.
-	 * @param CActiveRecord $model model which fields need to be inline editable
-	 * @param string $field name of field which need to be saved after edit
-	 * @param strign $value current value of the field (init value for textarea)
+     * @param CActiveRecord $model model which fields need to be inline editable
+     * @param string $field name of field which need to be saved after edit
+     * @param strign $value current value of the field (init value for textarea)
      * @param bool $enable
      * @param bool $return whether the rendering result should be returned
      * instead of being displayed to end users
-	 * @param string $updateUrl ajax POST requests will be sended to this url
+     * @param string $updateUrl ajax POST requests will be sended to this url
      * @return string the rendering result. 
      * Null if the rendering result is not required.
-	 */  
-	public static function textField($model, $field, $value, $enable = true, $return = false, $updateUrl = '/{className}/update/{primaryKey}') {
-		if (!$enable)
+     */  
+    public static function textField($model, $field, $value, $enable = true, $return = false, $updateUrl = '/{className}/update/{primaryKey}') {
+        if (!$enable)
             $fieldHtml = $value;
         else {
             static $registered;
@@ -32,10 +32,10 @@ class InlineEdit {
                 '</textarea>';
         }
         if($return)
-			return $fieldHtml;
+            return $fieldHtml;
         else
-			echo $fieldHtml;
-	}
+            echo $fieldHtml;
+    }
     
     /**
      * Binds .inactive and .active selectors with appropriate functions:
